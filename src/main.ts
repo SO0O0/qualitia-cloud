4b049c36d52e2d8538b588a12df33eb34bb46165
SO0O0 <hirawatasou@gmail.com>
2022-12-16 13:12:03 +0900

    :recycle: Convert to Nuxt

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/fonts.scss'
-import './assets/main.scss'
-import './assets/reset.scss'
-
-createApp(App).mount('#app')
